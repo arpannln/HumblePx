0792f1beae624320a77590e5608bed2ea5ec0097
Arpan NLN <arpannln@ucla.edu>
2018-02-05 20:56:58 -0800

    wrap up profile page basic css

--- frontend/components/users/user_profile.jsx
+++ frontend/components/users/user_profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
+import PhotoShowContainer from '../photos/photo_show_container';
 
 class UserProfile extends React.Component {
   constructor(props) {
@@ -17,10 +18,29 @@ class UserProfile extends React.Component {
 
   render () {
     const user = this.props.currentUser;
+    let photos = [];
+    if (this.props.users[user.id]) {
+      photos = this.props.users[user.id].photos;
+    }
+    console.log(photos);
     if (user) {
     return (<div className = "user-background">
      <img className="user-cover" src={user.cover_url}/>
      <img className="user-photo" src={user.img_url}/>
+       <div className="profile-all-photos">
+         {
+           photos.map( photo => (
+             <ul key={photo.id} className="profile-photo-container">
+               <Link to={`/photos/${photo.id}`}>
+                 <img className="profile-single-photo" src={photo.img_url} alt={photo.title}/>
+                 <br></br>
+               </Link>
+               <h3 className="profile-photo-caption">What a cute picture!</h3>
+             </ul>
+           ))
+         }
+         <Route exact path="/photos/:photoId" component={PhotoShowContainer} />
+       </div>
       </div>);
     } else {
       return (<div>

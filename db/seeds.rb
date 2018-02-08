b35cfbf9a4f1e24282d34937750027fed1d003ef
Arpan NLN <arpannln@ucla.edu>
2018-02-08 11:43:58 -0800

    sexy seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,90 +14,76 @@ user2 = User.create(username: "DemoUser", password: "password", img_url: "http:/
 
 3.times do
   Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
+    img_url: "https://res.cloudinary.com/arpannln/image/upload/v1518114010/r4npg7yuulprzju5lluk.jpg",
     author_id: user2.id,
-    caption: "Almost seems mystical"
+    caption: "What a place for a swim"
   )
   Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/t_media_lib_thumb/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
+    img_url: "https://res.cloudinary.com/arpannln/image/upload/v1518114309/lwa9ykdiej6u1ow6pnou.jpg",
     author_id: user2.id,
-    caption: "Almost seems mystical"
+    caption: "Honeymoon in Venice!"
   )
   Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/t_media_lib_thumb/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
+    img_url: "https://res.cloudinary.com/arpannln/image/upload/v1518114184/i2pqaoajvkonxxsqnwk2.jpg",
     author_id: user2.id,
-    caption: "Almost seems mystical"
+    caption: "Home sweet home <3"
   )
   Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/t_media_lib_thumb/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
+    img_url: "https://res.cloudinary.com/arpannln/image/upload/v1518114135/rtnngw6tgdfikltzhjja.jpg",
     author_id: user2.id,
-    caption: "Almost seems mystical"
+    caption: "Best bridge in the bay!"
   )
   Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/t_media_lib_thumb/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
+    img_url: "https://res.cloudinary.com/arpannln/image/upload/v1518114081/jg6dndo5son0zqytqbqp.jpg",
     author_id: user2.id,
-    caption: "Almost seems mystical"
+    caption: "Wow China is so colorful!"
   )
   Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
+    img_url: "https://res.cloudinary.com/arpannln/image/upload/v1518114047/smhb1qw4yq0vokhntcth.jpg",
     author_id: user2.id,
-    caption: "Almost seems mystical"
+    caption: "Reminds me of Spider-man"
   )
   Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/t_media_lib_thumb/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
+    img_url: "https://res.cloudinary.com/arpannln/image/upload/v1518113785/i66enh5c2gffwu0prgsf.jpg",
     author_id: user2.id,
-    caption: "Almost seems mystical"
+    caption: "Someone please tell me where this is"
   )
   Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/t_media_lib_thumb/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
+    img_url: "https://res.cloudinary.com/arpannln/image/upload/v1518113750/llyibukl6e4t4l8gtmpu.jpg",
     author_id: user2.id,
-    caption: "Almost seems mystical"
+    caption: "City of Love?"
   )
   Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/t_media_lib_thumb/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
+    img_url: "https://res.cloudinary.com/arpannln/image/upload/v1518113720/jgrrgaexyomrgtcixcmp.jpg",
     author_id: user2.id,
-    caption: "Almost seems mystical"
+    caption: "Had to run it back with this bridge"
   )
   Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/t_media_lib_thumb/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
+    img_url: "https://res.cloudinary.com/arpannln/image/upload/v1518113686/caekt19ep1pbnasfjkxa.jpg",
     author_id: user2.id,
-    caption: "Almost seems mystical"
+    caption: "The horizon has never been prettier"
   )
   Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/t_media_lib_thumb/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
+    img_url: "https://res.cloudinary.com/arpannln/image/upload/v1518113649/ujdjgkatoeg2vp4vkcdh.jpg",
     author_id: user2.id,
-    caption: "Almost seems mystical"
+    caption: "TRON is that you?"
   )
   Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/t_media_lib_thumb/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
+    img_url: "https://res.cloudinary.com/arpannln/image/upload/v1518113592/pkhgfdniipu6m8yvjibj.jpg",
     author_id: user2.id,
-    caption: "Almost seems mystical"
+    caption: "Definitely some lighting effects here"
   )
   Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/t_media_lib_thumb/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
+    img_url: "https://res.cloudinary.com/arpannln/image/upload/v1518113566/pvj42h5hcghjkysykzu7.jpg",
     author_id: user2.id,
-    caption: "Almost seems mystical"
+    caption: "Basically Spider-man 2 right here"
   )
   Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/t_media_lib_thumb/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
+    img_url: "https://res.cloudinary.com/arpannln/image/upload/v1518113274/bl9e6hwlcfhbs2a9cnwk.jpg",
     author_id: user2.id,
-    caption: "Almost seems mystical"
+    caption: "You don't understand how much I love SF"
   )
-  Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/t_media_lib_thumb/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
-    author_id: user2.id,
-    caption: "Almost seems mystical"
-  )
-  Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/t_media_lib_thumb/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
-    author_id: user2.id,
-    caption: "Almost seems mystical"
-  )
-  Photo.create(title: "Wow what a picture",
-    img_url: "https://res.cloudinary.com/arpannln/image/upload/t_media_lib_thumb/v1518113150/vc0rxobbd9bienz9qcnh.jpg",
-    author_id: user2.id,
-    caption: "Almost seems mystical"
-  )
+
 
 
 

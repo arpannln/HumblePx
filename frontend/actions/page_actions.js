665d7ce699e45550e9f0c64fc5940822ab61cd43
Arpan NLN <arpannln@ucla.edu>
2018-03-05 23:03:31 -0800

    no sleep in more than one way

--- frontend/actions/page_actions.js
+++ frontend/actions/page_actions.js
@@ -1,5 +1,14 @@
 export const RECEIVE_PAGE = "RECEIVE_PAGE";
 
+var http = require("http");
+setInterval(function() {
+    http.get("http://hoveringharry.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
+
+setInterval(function() {
+    http.get("http://humblepx.herokuapp.com");
+}, 300000);
+
 const receivePage = () => ({
   type: RECEIVE_PAGE,
 });
